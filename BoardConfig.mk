455a4b3405159d06b1e55bf7906e56bfb208d802
Nathan Chancellor <natechancellor@gmail.com>
2018-04-06 19:00:08 -0700

    wahoo: Build the kernel with Clang
    Google put a lot of work into getting this working, we should take
    advantage of it!
    
    Change-Id: I38195a101c4660688cd69bfe9e473e10ef24cabb
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_KERNEL_CONFIG := wahoo_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
