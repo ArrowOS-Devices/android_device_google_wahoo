2aa2410a0e77d325a68f0f8be85ebf4053e99471
Eamon Powell <eamonpowell@outlook.com>
2020-03-10 10:08:53 +1100

    wahoo: Build the vendor image
    Change-Id: Ie99dae74906e138813ae7bbd26f2d5b7e8268071

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,9 @@ BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 
 TARGET_COPY_OUT_VENDOR := vendor
 
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 524288000
+
 # Install odex files into the other system image
 BOARD_USES_SYSTEM_OTHER_ODEX := true
 
@@ -185,4 +188,5 @@ AB_OTA_PARTITIONS += \
     boot \
     system \
     vbmeta \
-    dtbo
+    dtbo \
+    vendor
