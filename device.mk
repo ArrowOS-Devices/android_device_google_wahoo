558944c26bb058e4ab97758a06cc38523cf77a3f
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:58:27 -0700

    wahoo: Use EXPENSIVE_RENDERING power hint for blurs
    We need to boost the GPU for smooth high-radius blurs as blurring is an
    expensive rendering job.
    
    Change-Id: Ia3968313601d2b04ce50c01ee61dc9944ee26187

--- device.mk
+++ device.mk
@@ -752,7 +752,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
 
 # Use /product/etc/fstab.postinstall to mount system_other.
 PRODUCT_PRODUCT_PROPERTIES += \
