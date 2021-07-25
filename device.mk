e91bc3360c1ffff2680596a80e9f2d6c467fdd92
ReallySnow <reallysnow233@gmail.com>
2021-07-25 17:53:55 +0800

    wahoo: Enable IMS and WFC prop

--- device.mk
+++ device.mk
@@ -782,6 +782,18 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
+
 # Build necessary packages for system
 PRODUCT_PACKAGES += \
     libhidltransport \
