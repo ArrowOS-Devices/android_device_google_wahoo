18a69004abecf3e5de486b34604420cc081b0a24
ReallySnow <reallysnow233@gmail.com>
2021-11-18 17:51:27 +0800

    wahoo: Set vendor security patch to 2020-10-05
    * wahoo EOL is 2020-10-05

--- device.mk
+++ device.mk
@@ -872,7 +872,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.gfx.driver.0=com.google.pixel.wahoo.gfxdrv
 
 # Setting vendor SPL
-VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+VENDOR_SECURITY_PATCH := 2020-10-05
 
 # Now Playing
 PRODUCT_PACKAGES += \
